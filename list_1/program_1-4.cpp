039b6a23429cdc3e2e0659788c8fb2f80c1e8e61
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-08-19 08:51:39 +0530

    Update program_1-4.cpp

--- list_1/program_1-4.cpp
+++ list_1/program_1-4.cpp
@@ -6,7 +6,7 @@ int main()
 	
 	cout<<" Mohammed Chuahan "<<endl;
 	cout<<"En_no:-220130318004 "<<endl;
-	cout<<"---------------------"<<endl;
+	cout<<"...................."<<endl;
 	cout<<"Enter the radius of circle : ";
 	cin>>r;
 	cout<<"Diameter of circle        : "<<3.14*2*r<<endl;
