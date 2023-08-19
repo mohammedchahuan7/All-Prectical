a715b1c4370ad3a9de38386cd1d036e27981cbd1
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-08-19 08:53:02 +0530

    Create program_5.cpp

---
+++ list_1/program_5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 10, temp;
+
+	  cout<<"Mohammed Chauhan "<<endl;
+	  cout<<"En_no:-220130318004 "<<endl;
+	  cout<<"...................."<<endl;
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
