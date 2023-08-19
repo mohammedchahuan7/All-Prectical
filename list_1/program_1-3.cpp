6c5cb803e2f767c96b5cfe22d4de7f2d5bffe21d
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-08-19 08:51:24 +0530

    Update program_1-3.cpp

--- list_1/program_1-3.cpp
+++ list_1/program_1-3.cpp
@@ -5,7 +5,7 @@ int main()
 	int l,w;
 	cout<<" Mohammed Chauhan "<<endl;
 	cout<<"En_no:-220130318004 "<<endl;
-	cout<<"---------------------"<<endl;
+	cout<<"...................."<<endl;
 	cout<<"Enter the lenght of rectangle : ";
 	cin>>l;
 	cout<<"Enter the width of rectangle : ";
