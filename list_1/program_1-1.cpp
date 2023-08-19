8df366814667a28a133d133ff90e442483c72663
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-08-19 08:50:55 +0530

    Update program_1-1.cpp

--- list_1/program_1-1.cpp
+++ list_1/program_1-1.cpp
@@ -5,7 +5,7 @@ int main()
 	float a,b;
 	cout<<"Mohammed Chauhan"<<endl;
 	cout<<"En_no:-220130318004 "<<endl;
-	cout<<"---------------------"<<endl;
+	cout<<"...................."<<endl;
 	cout<<"Enter the value of a  : ";
 	cin>>a;
 	cout<<"Enter the value of b  : ";
