34f0d06e8f35fb6a92e4ee28827cd2250f65fcc2
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-08-19 08:42:57 +0530

    Create program_3.cpp

---
+++ list_1/program_3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout<<"Mohammed Chauhan "<<endl;
+	  cout<<"En_no:-220130318004 "<<endl;
+    cout << "hello world";
+	  cout<<"---------------------"<<endl;
+  return 0;
+}
