32120b72e820dbca7504c782a28d49540fc69501
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-08-19 08:43:39 +0530

    Update program_3.cpp

--- list_1/program_3.cpp
+++ list_1/program_3.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main() 
 {
-    cout<<"Mohammed Chauhan "<<endl;
-	  cout<<"En_no:-220130318004 "<<endl;
-    cout << "hello world";
-	  cout<<"---------------------"<<endl;
+	cout<<"Mohammed Chauhan "<<endl;
+	cout<<"En_no:-220130318004 "<<endl;
+    	cout << "hello world";
+	cout<<"---------------------"<<endl;
   return 0;
 }
