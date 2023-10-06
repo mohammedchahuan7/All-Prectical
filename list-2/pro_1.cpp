997934ac259479dd0f3f7297eb39b62b8353e739
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-10-06 08:15:21 +0530

    pro_1.cpp

---
+++ list-2/pro_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int m = 10;// Global variable 
+main()
+{
+	// Scope resolution program 
+	cout<<"Mohammed Chauhan"<<endl;
+	cout<<"En_no:-220130318004 "<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"value of Global variable is m & its value  ::  "<<::m;// scop resolution 
+	return 0;
+}
