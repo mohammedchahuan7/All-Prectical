867847b57ffb9e6d2aae604c3a7982e215dfd97f
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-10-06 08:28:56 +0530

    pro_9.cpp

---
+++ list-2/pro_9.cpp
@@ -0,0 +1,15 @@
+ #include <iostream> 
+ using namespace std; 
+ int main()  
+ { 
+    int a,b; 
+     cout<<"En_no:-220130318004"<<endl; 
+      cout<<"Mohammed Chauhan"<<endl; 
+     cout << "Enter the first number: "; 
+     cin>>a; 
+     cout << "Enter the second number: "; 
+     cin>>b; 
+     int sum =a+b; 
+     cout << "Sum of " << a << " and " << b << " is: " << sum << endl; 
+     return 0; 
+ }
