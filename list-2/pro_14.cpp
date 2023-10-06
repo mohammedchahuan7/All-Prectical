ae4cbaab49bce95f76d6c360bc98a87a8eb3c5f9
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-10-06 08:37:03 +0530

    pro_14.cpp

---
+++ list-2/pro_14.cpp
@@ -0,0 +1,17 @@
+ #include<iostream>
+ using namespace std; 
+ inline int cube(int r) 
+ { 
+         return r*r*r; 
+ } 
+ int main() 
+ { 
+         int r; 
+         cout<<"En_no:-220130318004"<<endl; 
+         cout<<"Mohammed Chauhan"<<endl; 
+         cout<<"enter the number::"; 
+         cin>>r; 
+         cout<<"cube is ::"<<cube(r); 
+         return 0; 
+  
+ }
