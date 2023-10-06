9d10de97190cad7e217be5f162c3a5010ee36766
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-10-06 08:34:25 +0530

    pro_14.cpp

---
+++ list-2/pro_14.cpp
@@ -0,0 +1,19 @@
+ #include <iostream> 
+ using namespace std; 
+ class temp  
+ { 
+     public: 
+         void display(); 
+ }; 
+ void temp::display()  
+ { 
+     cout<<"En_no:-220130318004<<endl; 
+      cout<<"Mohammed Chauhan"<<endl; 
+     cout<"MY NAME IS VASU" <<endl; 
+ } 
+ int main()  
+ { 
+     temp t; 
+     t.display(); 
+     return 0; 
+ }
