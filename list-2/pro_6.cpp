3c751c33359149f6a9c4b7b37c70547098a04e34
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-10-06 08:22:56 +0530

    pro_6.cpp

---
+++ list-2/pro_6.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+ #include<iomanip> 
+ using namespace std;  
+ int main() 
+ { 
+         int a =2594; 
+         cout<<"220130318004"<<endl; 
+         cout<<"Mohammed Chauhan"<<endl; 
+         cout<<"The value of A=>"<<setw(10)<<setfill('*')<<a; 
+         return 0; 
+ }
