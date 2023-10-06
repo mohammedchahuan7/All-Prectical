cf7f4bcbf2d3479ce2a48cd9adf029dad7327bb4
mohammedchahuan7 <142320050+mohammedchahuan7@users.noreply.github.com>
2023-10-06 08:21:51 +0530

    pro_5.cpp

---
+++ list-2/pro_5.cpp
@@ -0,0 +1,14 @@
+ #include<iostream> 
+ #include<iomanip> 
+ using namespace std; 
+ int main() 
+ { 
+         int a=1234; 
+         float b=24.121234; 
+         cout<<"220130318004"<<endl; 
+         cout<<"Mohammed Chauhan"<<endl; 
+         cout<<"The value of a is :: "<<setfill('@')<<setw(5)<<a<<endl; 
+         cout<<"The value of b is :: "<<setprecision(5)<<b<<endl; 
+         return 0; 
+  
+ }
